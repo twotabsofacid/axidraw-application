eb668711050943d3e52b0cfcf45a7a5ef6a0c1f2
skiprox <seanmichaelscanlan@gmail.com>
2019-12-17 13:06:35 -0500

    Updating with file uploader

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,8 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-const {ipcRenderer} = require('electron');
+const { ipcRenderer } = require('electron');
+const { dialog } = require('electron').remote;
 
 class Renderer {
 	constructor() {
@@ -9,8 +10,10 @@ class Renderer {
 		this.addListeners();
 	}
 	storeValues() {
+		this.filename = '';
 		this.speedPendownElem = document.getElementById('speedPendownRange');
 		this.accelerationElem = document.getElementById('accelerationRange');
+		this.selectElem = document.getElementById('select');
 		this.submitElem = document.getElementById('submit');
 	}
 	addListeners() {
@@ -22,6 +25,11 @@ class Renderer {
 				document.getElementById(e.target.getAttribute('name')).innerText = val;
 			});
 		}
+		// Select button
+		this.selectElem.addEventListener('click', (e) => {
+			e.preventDefault();
+			this.selectFile();
+		});
 		// Submit button
 		this.submitElem.addEventListener('click', (e) => {
 			e.preventDefault();
@@ -35,11 +43,22 @@ class Renderer {
 			console.log(data);
 		});
 	}
+	selectFile() {
+		let newDialog = new dialog.showOpenDialog({
+			properties: ['openFile']
+		});
+		newDialog.then((value) => {
+			if (value.filePaths && typeof value.filePaths[0] == 'string') {
+				this.selectElem.innerText = value.filePaths[0];
+				this.filename = value.filePaths[0];
+			}
+		})
+	}
 	runAxidraw() {
 		let speedPendown = parseInt(this.speedPendownElem.innerText, 10);
 		let accel = parseInt(this.accelerationElem.innerText, 10);
 		ipcRenderer.send('run-axidraw', {
-			"filename": "somethig_cool.png",
+			"filename": this.filename,
 			"speed_pendown": speedPendown,
 			"accel": accel
 		});
